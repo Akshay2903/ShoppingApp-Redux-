5b8f245e4305d1835da06e3ec6607b3aa6301568
akshay2903 <akshaypal2903@gmail.com>
2023-04-27 00:32:16 +0530

    Fixing the Cart problems

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -6,13 +6,13 @@ const DUMMY_PRODUCTS = [
     id: 'p1',
     price: 6,
     title: 'My First Book',
-    description: 'The First Book I Ever Wrote',
+    description: 'The first book I ever wrote',
   },
   {
     id: 'p2',
-    price: 4,
+    price: 5,
     title: 'My Second Book',
-    description: 'The Second Book I Ever Wrote',
+    description: 'The second book I ever wrote',
   },
 ];
 
@@ -21,16 +21,15 @@ const Products = (props) => {
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        {DUMMY_PRODUCTS.map((product) =>(
-           <ProductItem
-           key = {product.id}
-           id = {product.id}
-           title = {product.title}
-           price= {product.price}
-           description= {product.description}
-         />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
         ))}
-       
       </ul>
     </section>
   );
