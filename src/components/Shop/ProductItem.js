bc00bb6280ad98d2bdfd233dee203e4de49b5c38
akshay2903 <akshaypal2903@gmail.com>
2023-04-27 00:02:52 +0530

    Doing Network calls and andling response the right way

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,21 +1,25 @@
 import { useDispatch } from 'react-redux';
+
 import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description, id } = props;
-
   const dispatch = useDispatch();
 
+  const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
     dispatch(
       cartActions.addItemToCart({
-      id,
-      price,
-      description
-    }));
+        id,
+        title,
+        price,
+      })
+    );
   };
 
   return (
